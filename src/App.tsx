6c383cbce62aff3b4de2293d04293c35e1e89056
Maurice <maculus1@gmail.com>
2025-10-05 14:13:55 -0400

    Build a responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+
+      <Box hideBelow="lg">  
+        <GridItem area="aside" bg="gold">
+          Aside 
+        </GridItem>
+      </Box>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  ); // 'Box' element and 'hideBelow' attribute added required for Chakra UI v3 // 'show' and 'below' for v2
 }
 
 export default App;
