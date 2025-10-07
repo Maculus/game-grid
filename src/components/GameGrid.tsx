1673be1e13bab22991984cc88b09b35ad9a5c430
Maurice <maculus1@gmail.com>
2025-10-07 02:54:49 -0400

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-// Define the structure of API response
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames(); // Destructure games and error from the custom hook
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+// Define the structure of API response
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+
+const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+
+    const controller = new AbortController(); 
+
+    apiClient
+      .get<FetchGamesResponse>("/games", {signal: controller.signal})
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if(err instanceof addEventListener) return;
+        setError(err.message)
+    });
+
+    return () => controller.abort(); // Cleanup function to abort the request on unmount
+  }, []);
+  
+  return { games, error };
+}
+export default useGames;
